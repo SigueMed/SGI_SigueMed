5307526bf9972b00bff7b0ecd5ebe401cc6f67d6
manuel.basurto <manuel.basurto@gmail.com>
2018-11-19 19:00:19 -0600

    Corrección Login

--- application/views/templates/headerMenu.php
+++ application/views/templates/headerMenu.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<? echo base_url();?>css/all.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/all.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<title>Login</title>
@@ -16,15 +16,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">SigueMED</a>
+      <a class="navbar-brand" href="#">SiguéMED</a>
     </div>
        <?php 
                 $FuncionesPerfil = $this->session->userdata('FuncionesPerfil');
+                
                 echo "<ul class='nav navbar-nav'>";
                 if ($FuncionesPerfil)
                 {
                     foreach ($FuncionesPerfil as $funcion)
                     {
+                        
                         if ($funcion['IdMenuPadre']==NULL)
                         {
                             echo "<li class='dropdown' ><a class='dropdown-toggle' data-toggle='dropdown' href='#'>". $funcion['DescripcionMenu'].'</a>';
@@ -36,19 +38,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     if ($ulFlag == False)
                                     {
                                        echo "<ul class='dropdown-menu'>";
+                                        $ulFlag=true;
                                     }
 
-                                        $ulFlag=true;
+                                       
                                         echo "<li><a href='".site_url().'/'.$SubFuncion['UrlMenu']."'>". $SubFuncion['DescripcionMenu'].'</a></li>';
 
 
                                 }
-                                if ($ulFlag==true)
+                                
+                            }
+                            if ($ulFlag==true)
                                 {
                                      echo "</ul>";
                                      $ulFlag = false;
                                 }
-                            }
                             echo '</li>';
                         }
                     }
