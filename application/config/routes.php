74ab8f10db6affb5c960d204f28fb13ad2f05681
PabloAlejandro4 <pablo1234delao@gmail.com>
2018-11-26 15:20:59 -0600

    Update routes.php
    Ruta de Notaremision

--- application/config/routes.php
+++ application/config/routes.php
@@ -65,3 +65,4 @@ $route['usuario/login'] = 'Usuario/login';
 $route['main/TableroPrincipal'] = 'Usuario/TableroPrincipal';
 
 //Nota Medica
+$route['NotaMedica/CrearNota'] = 'NotaMedica_Controller/CrearNotaRemision';
